33aa94b8b0fbba1c527144f5934a90c8dee159a9
18385963 <2013tim.g@gmail.com>
2018-11-30 00:33:52 -0500

    further updates network->train() and network->updateNetwork

--- Project1/network.cpp
+++ Project1/network.cpp
@@ -23,11 +23,31 @@ void network::train ( int &epochs, int batch_size, double eta ){
 	std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
 	std::shuffle(index.begin(), index.end(), rng);
 	
+	int *batch = new int[batch_size];
 	for (int i = 0 - batch_size; i < n; i += batch_size) {
-		int batch[2] = { i + batch_size, i + batch_size * 2 };
+		
+
+		for (int j = i + batch_size, int k = 0; j < i + batch_size * 2; j++, k++) {
+			// This loop populates the batch array with each element from the index
+			// j loops through each corresponding elements in index
+			// k loops through each batch element instead
+			batch[k] = index[j];
+			/*for testing purposes*/ std::cout << batch[1] << std::endl
+		}
+		this->updateNetwork(batch, batch_size,eta);
+
 		
 	}
+	delete[] batch;
+
+}
+
+void network::updateNetwork(int *batch, int batch_size, double eta) {
+	
+	for (int i = 0; i < batch_size; i++) {
 		
+	}
+
 
 }
 
@@ -36,9 +56,6 @@ void network::backprop(std::vector<std::vector<int>> &x, std::vector<int> &y) {
 
 }
 
-void network::update (){
-
-}
 
 void network::prediction() {
 
--- Project1/network.h
+++ Project1/network.h
@@ -6,18 +6,21 @@ public:
 	network(std::vector<int>& layers, std::vector<std::vector<int>>& data, std::vector<int>& values);
 	void backprop(std::vector<std::vector<int>>& x, std::vector<int>& y);
 	void train(int & epochs, int batch_size, double eta);
-	void update();
+	void updateNetwork(int * batch, int batch_size, double eta);
 	void prediction();
 	std::vector<double> sigmoid(std::vector<double> &z);
 	std::vector<double> dsigmoid(std::vector<double> &z);
 	void printLayers();
 private:
 	std::vector<int> nwlayers;
-	std::vector<int> biases;
-	std::vector<int> weights;
+	std::vector<std::vector<double>> biases; //storing all biases for each sigmoid neuron, layer by layer
+	std::vector<std::vector<double>> weights; //storing all weights for each sigmoid neuron, layer by layer
 	std::vector<std::vector<int>> x; // A vector storing all the initial x inputs
 	std::vector<int> y; //A vector storing all the corresponding y's for each x
+	std::vector<std::vector<double>> die_b; //storing the resulting gradient descent vector for b 
+	std::vector<std::vector<double>> die_n; //storing the resulting gradient desdcent vector for n
 	int nlayers;
+
 
 };
 

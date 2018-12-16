981f786443f7c025b63285214f343f1ed1c4ac0b
18385963 <2013tim.g@gmail.com>
2018-12-16 11:43:37 -0800

    initialized die_b and die_w values

--- Project1/network.cpp
+++ Project1/network.cpp
@@ -2,19 +2,19 @@
 #include "NNHelperFunctions.h"
 #include "network.h"
 
-network::network(std::vector<int> &layers, std::vector<std::vector<double>> &data, std::vector<double> &values ) {
+network::network(std::vector<int> &layers, std::vector<std::vector<double>> &data, std::vector<std::vector<double>> &values ) {
 	x = data;
 	y = values;
 	nwlayers = layers;
 	nlayers = layers.size();
 
-	//initializing biases 
+	//allocating biases 
 	biases.resize(layers.size() - 1); // -1 here because the first layer is an input layer
 	for (int i = 1; i < (layers.size()); i++) { // index starts at one because first layer is input layer
 		biases[i - 1].resize(layers[i]); //i - 1 here because we set i to be the layer we want to target, and i starts at 1 instead of 0
 	}
 
-	// initializing weights
+	// allocating weights
 	weights.resize(layers.size() - 1);
 	for (int i = 1; i < (layers.size()); i++) { // index starts at one because first layer is input layer 
 		weights[i - 1].resize(layers[i]);
@@ -23,8 +23,7 @@ network::network(std::vector<int> &layers, std::vector<std::vector<double>> &dat
 		}
 
 	}
-
-	//initializing die_b and die_w as zeor matrices with dimensions same as weights and biases
+	//allocating die_b and die_w as zeor matrices with dimensions same as weights and biases
 	die_b.resize(biases.size());
 	for (int i = 0; i < biases.size(); i++) {
 		die_b[i].resize(biases[i].size());
@@ -37,6 +36,13 @@ network::network(std::vector<int> &layers, std::vector<std::vector<double>> &dat
 			die_w[i][j].resize(weights[i][j].size());
 		}
 	}
+
+	//allcating z matrix
+	z.resize(biases.size());
+	for (int i = 0; i < biases.size(); i++) {
+		z[i].resize(biases[i].size());
+	}
+
 	//randomly assign values the biases and wieghts
 	std::random_device rd;     // only used once to initialise (seed) engine
 	std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
@@ -107,34 +113,55 @@ void network::updateNetwork(int *batch, int batch_size, double eta) {
 
 }
 
-void network::backprop(std::vector<double> &xx,  double &yy) {
+void network::backprop(std::vector<double> &xx, std::vector<double> &yy) {
 	//a copy of xx is made here instead of a reference since will be changing the values of the vector in this function
 
 	//Activate all neurons
-	double sigma_z = 0, z = 0, w = 0, b = 0, x = 0;
+	double wx = 0, zz = 0, w = 0, b = 0, x = 0;
 	activations[0] = xx;
 
 	for (int i = 0; i < weights.size(); i++) {
-	//each layer
+		//each layer
 		for (int j = 0; j < weights[i].size(); j++) {
-		//each neuron
-			sigma_z = 0; //initializing sigma_z
+			//each neuron
+			wx = 0; //initializing sigma_z
 			std::vector<double> &ws = weights[i][j];
 			b = biases[i][j];
-			
-			for (int k = 0; k<ws.size(); k++){
-			//each weight
+
+			for (int k = 0; k < ws.size(); k++) {
+				//each weight
 				w = ws[k];
 				x = activations[i][k];
-				z = w * x + b;
-	
-				sigma_z += z;
-				
+				wx += w * x;
+
 			}
 
-			activations[i + 1][j] = sigmoid(sigma_z);
+			zz = wx + b;
+			z[i][j] = zz; //store zz in z matix.
+			activations[i + 1][j] = sigmoid(zz);
 		}
 	}
+
+	//backward pass
+	std::vector<double> delta(yy.size(), 0);
+
+	for (int i = 0; i < delta.size(); i++) {
+		if (!(z.empty() && activations.empty())) {
+			zz = z[z.size() - 1][i];
+			delta[i] = (yy[i] - activations[activations.size() - 1][i]) * dsigmoid(zz);
+		}
+	}
+
+	//intialize die_b and die_w values
+	die_b[die_b.size() - 1] = delta;
+	for (int i = 0; i < die_w[die_w.size() - 1].size(); i++) {
+		for (int j = 0; j < die_w[die_w.size() -1][i].size(); j++) {
+			die_w[die_w.size() - 1][i][j] = delta[i]*activations[activations.size()-2][j];
+		}
+	}
+
+	int stop = 1; //stop here
+	
 }
 
 
@@ -143,20 +170,20 @@ void network::prediction() {
 }
 
 
-double network::sigmoid(double sigma_z) {
+double network::sigmoid(double z) {
 	//This function applies sigmoid neuron calcuations given a vector of z values.
 	double output; // This vector will hold the function output values
 
-	output = 1.0 / (1.0 + sigma_z);
+	output = 1.0 / (1.0 + z);
 
 	return output;
 }
 
-double network::dsigmoid(double sigma_z) {
+double network::dsigmoid(double z) {
 	//This is the derivative of the sigmoid function 
 	double output;
 
-	output = (1.0 / (1.0 + sigma_z)) / (1 - (1.0 / (1.0 + sigma_z)));
+	output = (1.0 / (1.0 + z)) / (1 - (1.0 / (1.0 + z)));
 
 
 	return output;
--- Project1/network.h
+++ Project1/network.h
@@ -3,10 +3,10 @@
 
 class network {
 public:
-	network(std::vector<int> &layers, std::vector<std::vector<double>> &data, std::vector<double> &values);
+	network(std::vector<int>& layers, std::vector<std::vector<double>>& data, std::vector<std::vector<double>>& values);
 	void train(int  epochs, int batch_size, double eta);
 	void updateNetwork(int * batch, int batch_size, double eta);
-	void backprop(std::vector<double>& xx, double & yy);
+	void backprop(std::vector<double>& xx, std::vector<double>& yy);
 	void prediction();
 	double sigmoid(double sigma_z);
 	double dsigmoid(double sigma_z);
@@ -17,7 +17,8 @@ private:
 	std::vector<std::vector<double>>  biases; //storing all biases for each sigmoid neuron, layer by layer
 	std::vector<std::vector<std::vector<double> > > weights; //storing all weights for each sigmoid neuron, layer by layer
 	std::vector<std::vector<double>> x; // A vector storing all the initial x inputs
-	std::vector<double> y; //A vector storing all the corresponding y's for each x
+	std::vector <std::vector<double> > y; //A vector storing all the corresponding y's for each x
+	std::vector<std::vector<double>> z; //a matrix storign all the z activation values layer by layer. Should have same dimensions as biases.
 	std::vector<std::vector<double>> die_b; //storing the resulting gradient descent vector for b 
 	std::vector<std::vector<std::vector<double>>> die_w; //storing the resulting gradient desdcent vector for w
 	std::vector<std::vector<double>> activations; //storing all the neuron actionvations in the backprop() function
